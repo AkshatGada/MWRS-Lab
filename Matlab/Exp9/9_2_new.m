9a6a60f419c24771621bbbefd2d212d4e1204e13
AkshatGada <96907514+AkshatGada@users.noreply.github.com>
2025-03-11 23:38:36 +0530

    Update 9_2_new.m

--- Matlab/Exp9/9_2_new.m
+++ Matlab/Exp9/9_2_new.m
@@ -7,21 +7,19 @@ clc; clear;
 
 %% Parameters
 EbNo_dB = 0:5:35;   % Eb/N0 range in dB
-Eb      = 1;        % Energy per bit
-sigma   = 1;        % Rayleigh fading parameter (variance = sigma^2)
+
 N       = 1e5;      % Number of bits (Monte Carlo trials)
 
 BER_sim = zeros(size(EbNo_dB));
 
 for i = 1:length(EbNo_dB)
-    No_over_2 = Eb * 10^(-EbNo_dB(i)/10);
+    No_over_2 = 10^(-EbNo_dB(i)/10);
     
-    alpha = sigma * sqrt(-2 * log(rand(1, N)));   % Rayleigh
+    alpha = sqrt(-2 * log(rand(1, N)));   % Rayleigh
     noise = sqrt(No_over_2) * randn(1, N);        % AWGN (real)
     
     y = alpha * sqrt(Eb) + noise;
     
-  
     decisions   = (y <= 0);
     num_errors  = sum(decisions);
     
@@ -29,7 +27,7 @@ for i = 1:length(EbNo_dB)
 end
 
 
-rho_b  = 10.^(EbNo_dB/10) * (sigma^2);
+rho_b  = 10.^(EbNo_dB/10) ;
 BER_th = 0.5 * (1 - sqrt(rho_b ./ (1 + rho_b)));
 
 semilogy(EbNo_dB, BER_sim, '-*', EbNo_dB, BER_th, '-o', 'LineWidth', 1.5);
